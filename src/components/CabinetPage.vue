ba8cf15f6c612ba89684b5f347caec810aabae06
ivansusaninmail@gmail.com <ivansusaninmail@gmail.com>
2024-04-14 21:26:01 +0200

    add new files

---
+++ src/components/CabinetPage.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <h1>Кабинет пользователя</h1>
+    <p><strong>Текущий путь маршрута:</strong> {{ $route.fullPath }}</p>
+    <button type="button" class="btn btn-primary" @click="openModal">
+      Подробнее
+    </button>
+    <ModalVue
+      v-if="isModalOpen"
+      title="Пользовательское соглашение"
+      @close="isModalOpen = false"
+    >
+      <div class="information">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae,
+          repellendus fugit. Perferendis molestiae reiciendis magni minus, saepe
+          voluptatem. Laborum temporibus quas eos minus placeat saepe nisi
+          nesciunt distinctio optio fugit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae,
+          repellendus fugit. Perferendis molestiae reiciendis magni minus, saepe
+          voluptatem. Laborum temporibus quas eos minus placeat saepe nisi
+          nesciunt distinctio optio fugit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae,
+          repellendus fugit. Perferendis molestiae reiciendis magni minus, saepe
+          voluptatem. Laborum temporibus quas eos minus placeat saepe nisi
+          nesciunt distinctio optio fugit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae,
+          repellendus fugit. Perferendis molestiae reiciendis magni minus, saepe
+          voluptatem. Laborum temporibus quas eos minus placeat saepe nisi
+          nesciunt distinctio optio fugit.
+        </p>
+      </div>
+    </ModalVue>
+
+    <midi-player
+      src="https://magenta.github.io/magenta-js/music/demos/melody.mid"
+      sound-font
+      visualizer="#myVisualizer"
+    >
+    </midi-player>
+    <midi-visualizer type="piano-roll" id="myVisualizer"></midi-visualizer>
+    <!-- <midi-player
+  src="https://susaninivan.github.io/midi/BUMER2.mid"
+  sound-font visualizer="#myVisualizer">
+</midi-player> -->
+    <midi-visualizer type="piano-roll" id="myVisualizer"></midi-visualizer>
+
+    <!-- <midi-player src="https://susaninivan.github.io/midi/Solveig.mid"></midi-player> -->
+    <midi-player
+      src="https://susaninivan.github.io/midi/Solveig.mid"
+      sound-font
+      visualizer="#myVisualizer"
+    >
+    </midi-player>
+    <midi-visualizer type="piano-roll" id="myVisualizer"></midi-visualizer>
+  </div>
+</template>
+
+<script>
+import ModalVue from "./ModalVue";
+export default {
+  components: { ModalVue },
+  name: "CabinetPage",
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalOpen = true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,392 @@
+<template>
+  <div class="container">
+    <!-- Контент здесь -->
+    <div class="main-content">
+      <!--Sidebar section -->
+      <aside class="sidebar">
+        <img src="../assets/img/vd.jpg" class="img-wrap" alt="photovd" />
+        <!--Contact section -->
+        <!-- <div class="contact" >
+        <h3 class="sidebar-title">Contacts</h3>
+        <div><span class="contact-type">C: </span><a class="contact-link" href="tel:+7 7777777777">+77777777777</a></div>
+        <div><span class="contact-type">E: </span><a class="contact-link" href="mailto:chornyiav@gmail.com">chornyiav@gmail.com</a></div>
+      </div> -->
+        <!--Tech Skills section -->
+        <!-- <div class="tech-skills">
+        <h3 class="sidebar-title">Tech Skills</h3>
+        <ul class="skills-list">
+          <li class="skills-list-item"> <span class="skills-list-item-text">HTML5</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">CSS3</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">GIT</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">WebPack</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">JavaScript</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">React.js</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">Node.js</span></li>
+        </ul>
+      </div> -->
+        <!--Soft Skills section -->
+        <!-- <div class="soft-skills">
+        <h3 class="sidebar-title">Soft Skills</h3>
+        <ul class="skills-list"> 
+          <li class="skills-list-item"> <span class="skills-list-item-text">Scrum</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">Agile</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">GTD</span></li>
+          <li class="skills-list-item"> <span class="skills-list-item-text">Teamwork</span></li>
+        </ul>
+      </div> -->
+      </aside>
+      <div class="my-section">
+        <!--about me section -->
+        <div class="about-me">
+          <h1 class="about-me-name">Курочкін Володимир Данилович</h1>
+          <h2 class="about-me-profession">(24. 11. 1946 - 24. 11. 2023)</h2>
+          <p class="about-me-description">
+            Володимир Данилович (24. 11. 1946, м. Ленінакан, нині Ґюмрі,
+            Вірменія) – матеріалознавець. Доктор хімічних наук (2009). Закін.
+            Київ. політех. інститут (1971). Від 1967 працює в Інституті проблем
+            матеріалознавства НАНУ (Київ): від 2010 – провідний науковий
+            співробітник Наук. інтереси пов’язані з експерим. дослідж. та матем.
+            моделюванням високотемператур. фіз.-хім. процесів у плазмі газових
+            розрядів і на її границі з електродами, випаро­вування елементів,
+            процесів ва­куум. конденсації бінар. систем.
+          </p>
+        </div>
+
+        <!--My projects section -->
+
+        <div class="my-projects">
+          <h3 class="my-content-title text-uppercase">Основні праці</h3>
+
+          <ul class="my-projects-list">
+            <li class="my-projects-item">
+              <span class="my-projects-text">
+                <p>
+                  Кинетика испарения в приближении теории переходного состояния
+                  // Докл. АН УССР. 1985. № 9 (спів­авт.);
+                </p>
+                <!-- <a class="my-projects-link" href="https://hellenglish.goit.global/">https://hellenglish.goit.global/</a
+            >........................................................<span class="my-projects-bracket">[</span> HTML5, CSS3 <span class="my-projects-bracket">]</span> -->
+              </span>
+            </li>
+            <li class="my-projects-item">
+              <span class="my-projects-text">
+                <p>
+                  Interaction of a Spark Discharge with W–Cu electrodes Alloyed
+                  by REE // High Tem­perature Materials and Processes. 2000.
+                  Vol. 19, № 6 (спів­авт.);
+                </p>
+                <!-- <a class="my-projects-link" href="https://cryptohub.goit.global/">https://cryptohub.goit.global/</a
+            >..........................................................<span class="my-projects-bracket">[</span> JavaScript <span class="my-projects-bracket">]</span> -->
+              </span>
+            </li>
+            <li class="my-projects-item">
+              <span class="my-projects-text">
+                <p>
+                  Особенности образования аргоносодержащих моле­кулярных ионов в
+                  криоохладжаемой плазме тлеющего разряда // УХЖ. 2003. Т. 69, №
+                  9;
+                </p>
+                <!-- <a class="my-projects-link" href="https://kidslike.goit.global/">https://kidslike.goit.global/</a
+            >.................................................................<span class="my-projects-bracket">[ </span> React.js, Node.js <span class="my-projects-bracket">]</span> -->
+              </span>
+            </li>
+            <li class="my-projects-item">
+              <span class="my-projects-text">
+                <p>
+                  Мас-спектри жевріючого розряду при мас-спектрометричному
+                  аналізі високолегованих сталей // ПорМ. 2008. № 3/4;
+                </p>
+                <!-- <a class="my-projects-link" href="https://kidslike.goit.global/">https://kidslike.goit.global/</a
+            >.................................................................<span class="my-projects-bracket">[ </span> React.js, Node.js <span class="my-projects-bracket">]</span> -->
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div class="work-experience">
+          <!--My Work Experience section -->
+          <div>
+            <h3 class="my-content-title text-uppercase">
+              Композиції у виконанні Курочка Володимир Данилович
+            </h3>
+
+            <!-- Company #1-->
+            <div class="work-experience-company">
+              <h6>
+                Усі композиції – це запис живого виконання на електронному
+                піаніно.
+              </h6>
+              <p class="my-work">Вересень 2023</p>
+              <ul class="company-list">
+                <li class="my-work-list">
+                  <h6 class="text-uppercase">Schubert-Serenade (F.Liszt)</h6>
+                  <!-- <midi-player sound-font  src="https://susaninivan.github.io/midi/01RECSNG-Serenada-Shu-List.mid"></midi-player> -->
+                <!-- <midi-player sound-font="https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus" src="https://susaninivan.github.io/midi/01RECSNG-Serenada-Shu-List.mid"></midi-player> -->
+                <midi-player sound-font src="https://susaninivan.github.io/midi/1.mid"></midi-player>
+
+                </li>
+                <li class="my-work-list">
+                  <h6 class="text-uppercase">E.Grieg Nocturne</h6>
+                  <!-- <midi-player sound-font  src="https://susaninivan.github.io/midi/01RECSNG-Serenada-Shu-List.mid"></midi-player> -->
+                <!-- <midi-player sound-font="https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus" src="https://susaninivan.github.io/midi/02RECSNG-Nokturn-Grieg.mid"></midi-player> -->
+                <midi-player sound-font src="https://susaninivan.github.io/midi/2.mid"></midi-player>
+
+                </li>
+                <li class="my-work-list">
+                  <h6 class="text-uppercase">E.Grieg Solveig's Song</h6>
+                  <!-- <midi-player sound-font  src="https://susaninivan.github.io/midi/01RECSNG-Serenada-Shu-List.mid"></midi-player> -->
+                <!-- <midi-player sound-font="https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus" src="https://susaninivan.github.io/midi/02RECSNG-Nokturn-Grieg.mid"></midi-player> -->
+                <midi-player sound-font src="https://susaninivan.github.io/midi/3.mid"></midi-player>
+
+                </li>
+
+                
+              
+              </ul>
+            </div>
+
+            <!-- Company #2-->
+            <!-- <div class="work-experience-company">
+            <h4>Manager Roga & <span class="orange"> Kopyta New</span></h4>
+            <p class="my-work">March 2015 - October 2018  <span class="work-experience-separator">|</span> Country</p>
+            <ul class="company-list">
+              <li class="my-work-list" >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </li>
+              <li class="my-work-list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              <li class="my-work-list">Risus commodo viverra maecenas.</li>
+            </ul>
+          </div> -->
+
+            <!-- Company #3-->
+            <!-- <div class="work-experience-company">
+            <h4>Manager Roga & <span class="orange">Kopyta LLC</span></h4>
+            <p class="my-work">June 2014 - February 2015 <span class="work-experience-separator">|</span> Country</p>
+            <ul class="company-list">
+              <li class="my-work-list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              <li class="my-work-list">Quis ipsum suspendisse ultrices gravida.</li>
+              <li class="my-work-list">Risus commodo viverra maecenas.</li>
+            </ul>
+          </div> -->
+          </div>
+        </div>
+
+        <!--Education section -->
+        <!-- <div class="education">
+        <h3 class="my-content-title">Education</h3>
+        <h4 class="orange">National University of Radioelectronics</h4>
+        <h5>Management</h5>
+        <p class="my-work">September 2009 - June 2014 <span class="work-experience-separator">|</span> Country</p>
+      </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/assets/styles/styles.scss";
+
+body {
+  background-color: #e5e5e5;
+}
+.img-wrap {
+  max-width: 300px;
+}
+.about-me-profession {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 17px;
+  color: black;
+  margin: 0 0 20px 0;
+}
+
+/*   about me section */
+
+.about-me-name {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 45px;
+  line-height: 55px;
+  color: #000000;
+  margin: 0 0 25px 0;
+}
+
+.about-me-description {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  color: #595959;
+  margin: 0 0 40px 0;
+}
+
+.about-me {
+}
+
+/*  My projects section */
+
+.my-projects {
+  margin: 0 0 50px 0;
+}
+
+.my-projects-list {
+  padding: 0 0 0 18px;
+}
+
+.my-projects-item {
+  color: black;
+  font-weight: 700;
+}
+
+.my-projects-link {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  color: #595959;
+}
+
+.my-projects-text {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  color: #595959;
+  // margin-left: 5px;
+}
+
+.my-projects-bracket {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 24px;
+  color: #000000;
+}
+
+/*  Work experience */
+
+.work-experience-separator {
+  color: #000;
+}
+
+.work-experience-company {
+  margin: 0 0 15px 0;
+}
+
+.company-list {
+  padding: 0 0 0 18px;
+}
+
+.my-work-list {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  color: #595959;
+}
+
+.my-work {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 15px;
+  color: #a8a8a8;
+}
+
+.main-content {
+  display: flex;
+  width: 1200px;
+  background-color: #fff;
+  margin: 40px auto;
+  box-shadow: 6px 6px 20px rgba(0, 0, 0, 0.1);
+}
+
+.my-content-title {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 27px;
+  color: #000000;
+  margin-bottom: 10px;
+}
+
+.my-section {
+  padding: 102px 88px 102px 88px;
+}
+
+.orange {
+  color: #fb6d3a;
+}
+
+/*Sidebar*/
+
+.sidebar {
+  background-color: #1e2939;
+  width: 370px;
+  padding: 40px 0;
+}
+
+.contact,
+.tech-skills,
+.soft-skills {
+  margin: 50px 0 0 40px;
+  color: #ffffff;
+}
+
+.sidebar-title {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 27px;
+}
+
+/*Sidebar Contact*/
+
+.contact-type {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 24px;
+  margin-right: 5px;
+}
+
+.contact-link {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  color: #565e6a;
+  text-decoration: none;
+}
+
+.skills-list {
+  padding: 0 0 0 17px;
+}
+
+.skills-list-item {
+  color: #fb6d3a;
+}
+
+.skills-list-item-text {
+  color: #ffffff;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+}
+</style>
